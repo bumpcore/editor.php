66b925aae0473b512b25929f416f8abf4319532f
megasteve19 <megasteve19@users.noreply.github.com>
2023-04-14 20:11:43 +0000

    Compiling Templates

--- resources/php/bootstrap-five/linktool.php
+++ resources/php/bootstrap-five/linktool.php
@@ -1,21 +1,21 @@
 <div class="bg-light d-flex mb-3 rounded overflow-hidden flex-column flex-sm-row">
     <img
-        alt="<?= $data('meta.title'); ?>"
+        alt="<?php echo $data('meta.title'); ?>"
         width="100%"
-        src="<?= $data('meta.image.url'); ?>"
+        src="<?php echo $data('meta.image.url'); ?>"
 		style="object-fit: cover"
     >
     <div class="m-4 p-4 bg-white rounded flex-2">
-        <h4 class="text-xl fw-semibold"><?= $data('meta.title'); ?></h4>
+        <h4 class="text-xl fw-semibold"><?php echo $data('meta.title'); ?></h4>
         <p class="mb-3 text-sm">
             <small>
                 <a
                     class="link-dark fw-bold"
-                    href="<?= $data('link'); ?>"
+                    href="<?php echo $data('link'); ?>"
                     target="_blank"
-                ><?= $data('link'); ?></a>
+                ><?php echo $data('link'); ?></a>
             </small>
         </p>
-        <p class="mb-0"><?= $data('meta.description'); ?></p>
+        <p class="mb-0"><?php echo $data('meta.description'); ?></p>
     </div>
 </div>
--- resources/php/tailwind/image.php
+++ resources/php/tailwind/image.php
@@ -1,5 +1,5 @@
 <img
-    alt="<?= $data('caption'); ?>"
-    src="<?= $data('file.url'); ?>"
+    alt="<?php echo $data('caption'); ?>"
+    src="<?php echo $data('file.url'); ?>"
     class="block rounded-xl mb-4"
 >
