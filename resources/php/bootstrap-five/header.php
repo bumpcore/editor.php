d469dcabb782aa72e26da34903377955406dedf2
megasteve19 <megasteve19@users.noreply.github.com>
2023-04-14 20:37:43 +0000

    Fix styling

--- resources/php/bootstrap-five/header.php
+++ resources/php/bootstrap-five/header.php
@@ -1,25 +1,25 @@
 <?php switch($data('level')):
     case (1): ?>
-        <h1 class="fw-bold mb-3"><?php echo $data('text'); ?></h1>
+        <h1 class="fw-bold mb-3"><?= $data('text'); ?></h1>
     <?php break; ?>
 
     <?php case (2): ?>
-        <h2 class="fw-bold mb-3"><?php echo $data('text'); ?></h2>
+        <h2 class="fw-bold mb-3"><?= $data('text'); ?></h2>
     <?php break; ?>
 
     <?php case (3): ?>
-        <h3 class="fw-bold mb-3"><?php echo $data('text'); ?></h3>
+        <h3 class="fw-bold mb-3"><?= $data('text'); ?></h3>
     <?php break; ?>
 
     <?php case (4): ?>
-        <h4 class="fw-semibold mb-3"><?php echo $data('text'); ?></h4>
+        <h4 class="fw-semibold mb-3"><?= $data('text'); ?></h4>
     <?php break; ?>
 
     <?php case (5): ?>
-        <h5 class="fw-semibold mb-3"><?php echo $data('text'); ?></h5>
+        <h5 class="fw-semibold mb-3"><?= $data('text'); ?></h5>
     <?php break; ?>
 
     <?php case (6): ?>
-        <h6 class="fw-semibold mb-1"><?php echo $data('text'); ?></h6>
+        <h6 class="fw-semibold mb-1"><?= $data('text'); ?></h6>
     <?php break; ?>
 <?php endswitch; ?>
--- resources/php/tailwind/header.php
+++ resources/php/tailwind/header.php
@@ -1,25 +1,25 @@
 <?php switch($data('level')):
     case (1): ?>
-        <h1 class="text-5xl font-bold mb-4"><?php echo $data('text'); ?></h1>
+        <h1 class="text-5xl font-bold mb-4"><?= $data('text'); ?></h1>
     <?php break; ?>
 
     <?php case (2): ?>
-        <h2 class="text-3xl font-bold mb-4"><?php echo $data('text'); ?></h2>
+        <h2 class="text-3xl font-bold mb-4"><?= $data('text'); ?></h2>
     <?php break; ?>
 
     <?php case (3): ?>
-        <h3 class="text-2xl font-bold mb-2"><?php echo $data('text'); ?></h3>
+        <h3 class="text-2xl font-bold mb-2"><?= $data('text'); ?></h3>
     <?php break; ?>
 
     <?php case (4): ?>
-        <h4 class="text-xl font-semibold mb-2"><?php echo $data('text'); ?></h4>
+        <h4 class="text-xl font-semibold mb-2"><?= $data('text'); ?></h4>
     <?php break; ?>
 
     <?php case (5): ?>
-        <h5 class="text-base font-semibold mb-2"><?php echo $data('text'); ?></h5>
+        <h5 class="text-base font-semibold mb-2"><?= $data('text'); ?></h5>
     <?php break; ?>
 
     <?php case (6): ?>
-        <h6 class="text-sm font-semibold mb-1"><?php echo $data('text'); ?></h6>
+        <h6 class="text-sm font-semibold mb-1"><?= $data('text'); ?></h6>
     <?php break; ?>
 <?php endswitch; ?>
