843d0279bc5b1996b62d3e9493171c41306e32e9
megasteve19 <megasteve19@users.noreply.github.com>
2023-06-11 15:06:13 +0000

    Compiling Templates

--- resources/php/bootstrap-five/embed.php
+++ resources/php/bootstrap-five/embed.php
@@ -1,14 +1,14 @@
 <div class="mb-3 bg-light p-4 rounded">
     <iframe
         class="block mb-3 rounded bg-white"
-        height="<?= $data('height'); ?>"
+        height="<?php echo $data('height'); ?>"
         loading="lazy"
-        src="<?= $data('embed'); ?>"
+        src="<?php echo $data('embed'); ?>"
         width="100%"
     ></iframe>
 
     <p class="bg-white p-4 rounded mb-1">
-        <?= $data('caption'); ?>
+        <?php echo $data('caption'); ?>
 
     </p>
 
@@ -16,9 +16,9 @@
         <small>
             <a
                 class="link-secondary"
-                href="<?= $data('source'); ?>"
+                href="<?php echo $data('source'); ?>"
 				target="_blank"
-            ><?= $data('source'); ?></a>
+            ><?php echo $data('source'); ?></a>
         </small>
     </p>
 
