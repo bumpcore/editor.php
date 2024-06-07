14227f5d17acc916cb60fc0dce6a00ef4ae8c27d
megasteve19 <megasteve19@users.noreply.github.com>
2024-06-07 21:00:22 +0000

    Compiling Templates

--- resources/php/tailwind/personality.php
+++ resources/php/tailwind/personality.php
@@ -1,20 +1,20 @@
 <div class="bg-gray-100 flex mb-4 rounded-xl overflow-hidden flex-col sm:flex-row">
     <img
-        alt="<?= $data('name'); ?>"
+        alt="<?php echo $data('name'); ?>"
         class="sm:max-w-[16rem] object-cover grow sm:grow-0"
-        src="<?= $data('photo'); ?>"
+        src="<?php echo $data('photo'); ?>"
     >
     <div class="m-4 p-4 bg-white rounded-xl flex-1">
-        <h4 class="text-xl font-semibold"><?= $data('name'); ?></h4>
+        <h4 class="text-xl font-semibold"><?php echo $data('name'); ?></h4>
         <p class="mb-2 text-sm">
             <small>
                 <a
                     class="text-black hover:text-gray-600 font-bold"
-                    href="<?= $data('link'); ?>"
+                    href="<?php echo $data('link'); ?>"
                     target="_blank"
-                ><?= $data('link'); ?></a>
+                ><?php echo $data('link'); ?></a>
             </small>
         </p>
-        <p><?= $data('description'); ?></p>
+        <p><?php echo $data('description'); ?></p>
     </div>
 </div>
